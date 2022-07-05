6ba660a603380cff77cbfeff839fbcc102306bd3
Yoonitech <85844438+Yoonitech@users.noreply.github.com>
2022-07-06 01:24:51 +0430

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,44 @@
+
+console.log('hi');
+function onLinkClick6() {
+  var my_element = document.getElementById("6");
+
+  my_element.scrollIntoView(false);
+  // will scroll to 4th h3 element
+}
+function onLinkClick1() {
+  var my_element = document.getElementById("1");
+
+  my_element.scrollIntoView(false);
+  // will scroll to 4th h3 element
+}
+function onLinkClick7() {
+  var my_element = document.getElementById("7");
+
+  my_element.scrollIntoView(false);
+  // will scroll to 4th h3 element
+}
+function onLinkClick5() {
+  var my_element = document.getElementById("5");
+
+  my_element.scrollIntoView(false);
+  // will scroll to 4th h3 element
+}
+function onLinkClick4() {
+  var my_element = document.getElementById("4");
+
+  my_element.scrollIntoView(false);
+  // will scroll to 4th h3 element
+}
+function onLinkClick3() {
+  var my_element = document.getElementById("3");
+
+  my_element.scrollIntoView(false);
+  // will scroll to 4th h3 element
+}
+function onLinkClick2() {
+  var my_element = document.getElementById("2");
+
+  my_element.scrollIntoView(false);
+  // will scroll to 4th h3 element
+}
